a8fde8ad5b6c24fb88c93e2541de1aeb459a72bb
Mattias Rost <rrostt@gmail.com>
2013-02-09 23:39:36 +0000

    Added initial docs to protodb.php

--- protodb.php
+++ protodb.php
@@ -1,5 +1,63 @@
 <?php
 
+/**
+		ProtoDB v0.1.
+		Library for simple and quick use of databases when prototyping new applications and service.
+		
+		Having to properly setup a database structure and write queries for creating and operating tables, can sometimes
+		get in the way of creativity. This tool attempts to get that out of the way by setting up the database structure
+		as you use it. When inserting something into a table for the first time, will create the table and add columns as
+		it appears that you want there to be tables. Example:
+		
+			DB::insert("books", array("title" => "Mobility is the Message", "author" => "Mattias Rost"));
+		
+		The above statement will start by making sure there is a table with the name "books". If not, it will create one with
+		a BIGINT `id` as a primary key. It will then check if the columns "title" and "author" exists, and if not it will
+		add the columns to the database structure, before inserting a new row.
+		A simple query from the database is easily done through
+		
+			DB::get("books");
+		
+		or
+		
+			DB::get("books", array("author" => "Mattias Rost"));
+		
+		Furthermore, it includes a REST-ful API that allow your client app to directly store stuff in a database on your server.
+		There is also a javascript-library included, that let you do all your protodb'ing from within a browser.
+
+		This file includes
+			1) a PHP library for database storage, 
+			2) a REST-ful api for use remotely, and
+			3) a javascript library for use together with jQuery.
+
+		Usage
+		-----
+		
+		From PHP:
+			create a db.php file that establishes a mysql database connection and selects database.
+			include('protodb.php');
+			Use the functions from DB:: which is a class with static functions.
+			
+		From Javascript:
+			<script src="protodb.php?_js"></script>
+			<script>
+				protodb.insert("books", {author:"Mattias Rost", title:"ProtoDB for dummies"});
+				protodb.get("books", {author:"Mattias Rost"}, function(rows) {
+					rows.forEach(function(row) {
+						console.log(row);
+					});
+				});
+			</script>
+			
+		From other external client:
+			To insert, HTTP POST with _protodb variable set, and "cmd=insert", "table=books", "values={\"author\":\"Mattias Rost\", \"title\":\"ProtoDB for dummies\"}"
+			To get, HTTP GET with _protodb, and "cmd=get", and optional where, json-string.
+
+ */
+
+/*
+Make sure you have a mysql database connection. I do it in a separate file called db.php.
+*/
 require_once('db.php');
 
 class DB {
@@ -74,6 +132,10 @@ class DB {
 		);
 	}
 
+	/*
+		allows tables to be specified such that "users < userinfo(users.id=userinfo.userid)" will do
+		a users LEFT JOIN userinfo ON users.id=userinfo.userid.
+	*/
 	function parseTableStatement($table) {
 		$parts = explode(" ", $table);
 		if (count($parts)>1) {
@@ -224,11 +286,6 @@ class DB {
 	}
 }
 
-//DB::config( array("prefix" => "dbtest_") );
-//DB::drop();
-//DB::insert( "users", array( "name" => "mattias" ));
-//DB::set("users", null, array( "age" => "12" ));
-//echo json_encode(DB::get("users"));
 
 if (isset($_REQUEST['_protodb'])) {
 	DB::config( array( "prefix" => $dbprefix ));
@@ -307,11 +364,11 @@ var protodb = (function() {
 }
 
 if (isset($_REQUEST['_test'])) {
+	$dbprefix = "test_";
 	DB::config(array( "prefix" => $dbprefix ));
 
-	$table = "rooms < room_updates(rooms.id=room_updates.room_id) < people(room_updates.person_id=people.id)"; //$_REQUEST['table'];
-//	DB::parseTableStatement($table);
-	print_r(DB::get($table));
+	DB::insert("test", array("value" => "3"));
+	print_r(DB::get("test"));
 }
 
 ?>
